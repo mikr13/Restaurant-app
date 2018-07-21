6a0210e92cf61afd397fbaa25eee3d30cad3d39c
Mihir Kumar <mihirpandey.13@gmail.com>
2018-07-21 16:48:01 +0530

    conFusion Single Page Application update Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Dish } from '../shared/dish.model';
+
+// was not needed before in same page
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -9,13 +14,20 @@ import { Dish } from '../shared/dish.model';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input() dish: Dish;
+  // @Input() dish: Dish; previously when in same page
+  dish: Dish;
 
-  selectedDish = this.dish;
+  // selectedDish = this.dish; previously nothing in constructor and ngOnInit() {}
 
-  constructor() { }
+  constructor(private dishservice: DishService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
